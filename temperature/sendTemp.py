ec570ba48c935b3f95dbaefb2005ba9c60374f2f
astatsuya <sharekobekk@gmail.com>
2020-07-28 01:04:35 +0900

    set schedule

--- temperature/sendTemp.py
+++ temperature/sendTemp.py
@@ -9,6 +9,7 @@ sys.path.append("..")
 import environment
 sys.path.append("../DHT11_Python")
 import dht11
+import schedule
 
 # gitignoreしたファイルで環境変数（webhooksのURL）を生成
 environment.writeSlackIncomingUrl()
@@ -25,49 +26,58 @@ url = os.environ["SLACK_INCOMING_URL"]
 userId = "UTWME81C6"
 count = 0
 
-try:
-    while True:
-        result = instance.read()
-        if result.is_valid():
-            now = datetime.datetime.now()
-            convertedNow = now.strftime("%Y/%m/%d %H:%M")
-            
-            print("Last valid input: " + str(convertedNow))
+lastValidMessage = "no record"
+def getTemperatureAndHumidity():
+    result = instance.read()
+    now = datetime.datetime.now()
+    convertedNow = now.strftime("%Y/%m/%d %H:%M")
 
-            print("Temperature: %-3.1f C" % result.temperature)
-            print("Humidity: %-3.1f %%" % result.humidity)
-            count += 1
-            
-            tempAndHumidMessage = "{time}\n気温：{temp}度\n湿度：{humid}%".format(time = convertedNow,temp = result.temperature, humid = result.humidity)
-            
-            payload = 0
-            print(count)
-            if count >= 30:                        
-                # 定時投稿
-                payload = {"text": "<@{0}> 30分に一度の定時投稿。現在のはるきちのおうち\n\n{1}".format(userId, tempAndHumidMessage)}
-                response = requests.post(url, json=payload)
-                count = 0
-                print(response.status_code, response.text)
-            else:
-                # 温度変化検知
-                if result.temperature <= 25.0:
-                    payload = {"text": "<@{0}> はるきちのおうちが寒い！エアコンをオフにしてください！！\n\n{1}".format(userId, tempAndHumidMessage)}
-                
-                if result.temperature >= 30:
-                    payload = {"text": "<@{0}> はるきちのおうちが暑い！エアコンを入れてください！！\n\n{1}".format(userId, tempAndHumidMessage)}
-                    
-                if result.humidity <= 60:
-                    payload = {"text": "<@{0}> はるきちのおうちが乾燥してる！お水を上げてください！！\n\n{1}".format(userId, tempAndHumidMessage)}
-                    
-                if result.humidity >= 80:
-                    payload = {"text": "<@{0}> はるきちのおうちが湿気てる！！飼い主が不快な思いをしているかもしれません。\n\n{1}".format(userId, tempAndHumidMessage)}
-                
-                if payload != 0:
-                    response = requests.post(url, json=payload)
-                    print(response.status_code, response.text)
+    if result.is_valid():
+        print("Last valid input: " + str(convertedNow))
+        print("Temperature: %-3.1f C" % result.temperature)
+        print("Humidity: %-3.1f %%" % result.humidity)
+        lastValidMessage = "{time}\n気温：{temp}度\n湿度：{humid}%".format(time = convertedNow,temp = result.temperature, humid = result.humidity)
+        return result, lastValidMessage
+    else:
+        print("invalid input: " + str(convertedNow))
+
+def periodicalRequest(tempAndHumidMessage):
+    payload = {"text": "30分に一度の定時投稿。現在のはるきちのおうち\n\n{1}".format(userId, tempAndHumidMessage)}
+    response = requests.post(url, json=payload)
+    print(response.status_code, response.text)
+    
+def anomalyDetectRequest(result, tempAndHumidMessage):
+    if result.temperature <= 25.0:
+        payload = {"text": "<@{0}> はるきちのおうちが寒い！エアコンをオフにしてください！！\n\n{1}".format(userId, tempAndHumidMessage)}
+
+    if result.temperature >= 30:
+        payload = {"text": "<@{0}> はるきちのおうちが暑い！エアコンを入れてください！！\n\n{1}".format(userId, tempAndHumidMessage)}
+    
+    if result.humidity <= 60:
+        payload = {"text": "<@{0}> はるきちのおうちが乾燥してる！お水を上げてください！！\n\n{1}".format(userId, tempAndHumidMessage)}
+
+    if result.humidity >= 80:
+        payload = {"text": "<@{0}> はるきちのおうちが湿気てる！！飼い主が不快な思いをしているかもしれません。\n\n{1}".format(userId, tempAndHumidMessage)}
+    
+    if "payload" in locals():
+        response = requests.post(url, json=payload)
+        print(response.status_code, response.text)
+
+def periodicalLogging():
+    result = getTemperatureAndHumidity()
+    if result != None:
+        periodicalRequest(result[1])
         
-        time.sleep(1 * 60)
+def anormalLogging():
+    result = getTemperatureAndHumidity()
+    if result != None:
+        anomalyDetectRequest(result[0], result[1])
+            
+schedule.every(1).minutes.do(anormalLogging)
+schedule.every().hour.at("00:00").do(periodicalLogging)
+schedule.every().hour.at("30:00").do(periodicalLogging)
 
-except KeyboardInterrupt:
-    print("Cleanup")
-    GPIO.cleanup()
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
