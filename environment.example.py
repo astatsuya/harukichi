98783910ebdf549bac0db786d5c2dae3d1206533
astatsuya <sharekobekk@gmail.com>
2020-07-28 22:01:19 +0900

    change settings

--- environment.example.py
+++ environment.example.py
@@ -2,3 +2,7 @@ import os
 
 def writeSlackIncomingUrl():
     os.environ["SLACK_INCOMING_URL"] = "URL"
+    
+def writeSlackUserId():
+    os.environ["SLACK_USER_ID"] = "USERID"
+    
--- temperature/sendTemp.py
+++ temperature/sendTemp.py
@@ -11,8 +11,9 @@ sys.path.append("../DHT11_Python")
 import dht11
 import schedule
 
-# gitignoreしたファイルで環境変数（webhooksのURL）を生成
+# gitignoreしたファイルで環境変数（slack webhooksのURLとID）を生成
 environment.writeSlackIncomingUrl()
+environment.writeSlackUserId()
 
 # initialize GPIO
 GPIO.setwarnings(True)
@@ -23,8 +24,11 @@ instance = dht11.DHT11(pin=14)
 
 # slack-incoming-hookds url
 url = os.environ["SLACK_INCOMING_URL"]
-userId = "UTWME81C6"
-count = 0
+userId = os.environ["SLACK_USER_ID"]
+maxTemp = 30
+minTemp = 25
+maxHumid = 85
+minHumid = 60
 
 lastValidMessage = "no record"
 lastPostTime = None
@@ -50,16 +54,16 @@ def periodicalRequest(tempAndHumidMessage):
     print(response.status_code, response.text)
     
 def anomalyDetectRequest(result, tempAndHumidMessage):
-    if result.temperature <= 25.0:
+    if result.temperature <= minTemp:
         payload = {"text": "<@{0}> はるきちのおうちが寒い！エアコンをオフにしてください！！\n\n{1}".format(userId, tempAndHumidMessage)}
 
-    if result.temperature >= 30:
+    if result.temperature >= maxTemp:
         payload = {"text": "<@{0}> はるきちのおうちが暑い！エアコンを入れてください！！\n\n{1}".format(userId, tempAndHumidMessage)}
     
-    if result.humidity <= 60:
+    if result.humidity <= minHumid:
         payload = {"text": "<@{0}> はるきちのおうちが乾燥してる！お水を上げてください！！\n\n{1}".format(userId, tempAndHumidMessage)}
 
-    if result.humidity >= 80:
+    if result.humidity >= maxHumid:
         payload = {"text": "<@{0}> はるきちのおうちが湿気てる！！飼い主が不快な思いをしているかもしれません。\n\n{1}".format(userId, tempAndHumidMessage)}
     
     if "payload" in locals():
@@ -83,7 +87,6 @@ def anomalyDetectRequest(result, tempAndHumidMessage):
 
 def periodicalLogging():
     result = getTemperatureAndHumidity("定時実行")
-    print(lastValidMessage)
     periodicalRequest(lastValidMessage)
         
 def anormalLogging():
@@ -92,7 +95,6 @@ def anormalLogging():
         anomalyDetectRequest(result[0], result[1])
             
 schedule.every(1).minutes.do(anormalLogging)
-
 schedule.every().hour.at("00:00").do(periodicalLogging)
 schedule.every().hour.at("30:00").do(periodicalLogging)
 
